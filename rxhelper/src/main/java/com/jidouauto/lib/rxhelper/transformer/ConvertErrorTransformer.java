e2124dd88129adb5ae75786319188535112f4f60
eddiezhang <eddiezhang@jidouauto.com>
2020-09-22 19:10:52 +0800

    support convert the error of completable stream
    Change-Id: Id813915523ac5d8ea26157aebe7cdb3f9d768c2f

--- rxhelper/src/main/java/com/jidouauto/lib/rxhelper/transformer/ConvertErrorTransformer.java
+++ rxhelper/src/main/java/com/jidouauto/lib/rxhelper/transformer/ConvertErrorTransformer.java
@@ -4,6 +4,9 @@ import com.jidouauto.lib.rxhelper.ErrorConverter;
 
 import org.reactivestreams.Publisher;
 
+import io.reactivex.Completable;
+import io.reactivex.CompletableSource;
+import io.reactivex.CompletableTransformer;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableTransformer;
 import io.reactivex.Maybe;
@@ -19,6 +22,7 @@ import io.reactivex.SingleTransformer;
 class ConvertErrorTransformer<T> implements ObservableTransformer<T, T>,
         FlowableTransformer<T, T>,
         SingleTransformer<T, T>,
+        CompletableTransformer,
         MaybeTransformer<T, T> {
 
     private ErrorConverter<? extends Exception> errorConverter;
@@ -48,6 +52,14 @@ class ConvertErrorTransformer<T> implements ObservableTransformer<T, T>,
         return upstream
                 .onErrorResumeNext(throwable -> {
                     return Observable.error(errorConverter.convert(throwable));
+                });
+    }
+
+    @Override
+    public CompletableSource apply(Completable upstream) {
+        return upstream
+                .onErrorResumeNext(throwable -> {
+                    return Completable.error(errorConverter.convert(throwable));
                 });
     }
 
